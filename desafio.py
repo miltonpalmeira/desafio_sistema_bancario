3a75df4a1c9297a081533376567175ef1a57a5b6
Milton Palmeira <lpsantana_@hotmail.com>
2024-06-18 21:12:41 -0300

    desafio python

---
+++ desafio.py
@@ -0,0 +1,67 @@
+menu = """
+
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[0] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(menu)
+    print(opcao)
+
+    if opcao == "1":
+        valor = float(input("Digite o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+            print("Depósito efetuado com sucesso!")
+        else:
+            print("Valor informado inválido. Digite apenas valores positivos.")
+
+    elif opcao == "2":
+        valor = float(input("Digite o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Você não possui saldo suficiente.")
+
+        elif excedeu_limite:
+            print("O valor do saque excede o limite.")
+
+        elif excedeu_saques:
+            print("Número máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+            print("Saque efetuado com sucesso!")
+        else:
+            print("Valor informado inválido. Digite apenas valores positivos.")
+
+    elif opcao == "3":
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+
+    elif opcao == "0":
+        break
+
+    else:
+        print("Opção inválida! SElecione novamente a operação desejada.")
